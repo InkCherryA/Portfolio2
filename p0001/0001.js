7a2c377de22d79d991b78e22d21b1622f3f69374
InkCherryA <anniehe1126@gmail.com>
2023-01-12 01:33:09 -0500

    Commit 2

--- p0001/0001.js
+++ p0001/0001.js
@@ -1,42 +1,55 @@
-const html = document.documentElement;
-const canvas = document.getElementById("flip2");
-const context = canvas.getContext("2d");
 
-const frameCount = 10;
-const currentFrame = index => (
-  `./src/${index.toString().padStart(4, '0')}img.png`
-)
+//获取轮播容器对象 和 分页指示器元素对象
+var swiper = document.getElementById('fb1');
+//播放下一张图片的方法
+function changeAfter() {
+	//1. 删除轮播容器中第一个子元素，并返回被删除的元素对象
+	//swiper.children 返回swiper元素下的所有子元素
+	var child = swiper.removeChild(swiper.children[0]);
+	//2. 将被删除元素追加到轮播容器的末尾
+	swiper.appendChild(child);
 
-const preloadImages = () => {
-  for (let i = 1; i < frameCount; i++) {
-    const img = new Image();
-    img.src = currentFrame(i);
-  }
-};
 
-const img = new Image()
-img.src = currentFrame(1);
-canvas.width=1158;
-canvas.height=770;
-img.onload=function(){
-  context.drawImage(img, 0, 0);
 }
 
-const updateImage = index => {
-  img.src = currentFrame(index);
-  context.drawImage(img, 0, 0);
+//播放上一张图片的方法
+function changeBefore() {
+	//1. 获取最后一张在轮播容器中的的下标
+	var index = swiper.children.length - 1;
+	//2. 把最后一张删除，并返回被删除的元素对象
+	var child = swiper.removeChild(swiper.children[index]);
+	//3. 将被删除元素添加到第一张之前
+	swiper.insertBefore(child, swiper.children[0]);
+
 }
 
-window.addEventListener('scroll', () => {  
-  const scrollTop = html.scrollTop;
-  const maxScrollTop = html.scrollHeight - window.innerHeight;
-  const scrollFraction = scrollTop / maxScrollTop;
-  const frameIndex = Math.min(
-    frameCount - 1,
-    Math.ceil(scrollFraction * frameCount)
-  );
-  
-  requestAnimationFrame(() => updateImage(frameIndex + 1))
-});
 
-preloadImages()
+
+
+var handlerWheel = throttle(scrollMove, 100)
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/mousewheel_event#The_detail_property
+// firefox的页面滚动事件其他浏览器不一样
+if (navigator.userAgent.toLowerCase().indexOf('firefox') === -1) {
+	document.addEventListener('mousewheel', handlerWheel)
+} else {
+	document.addEventListener('DOMMouseScroll', handlerWheel)
+}
+function scrollMove() {
+	var e=e||window.event;
+	if (e.deltaY > 0) {
+    	changeAfter();
+	}else {
+    	changeBefore();
+	}
+}
+function throttle(func, timeFrame) {
+	var lastTime = 0;
+	return function () {
+		var now = Date.now();
+		if (now - lastTime >= timeFrame) {
+			func();
+			lastTime = now;
+		}
+	};
+}
+
